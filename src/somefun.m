0b689181e3e1291bd4d2f89bd1726145696fc4d0
Andy Campbell <47190204+acampbel@users.noreply.github.com>
2023-03-31 13:22:16 -0400

    Update somefun.m

--- src/somefun.m
+++ src/somefun.m
@@ -1,5 +1,11 @@
 function out = somefun(in)
 
+unsetenv("MLM_WEB_LICENSE")
+unsetenv("MLM_WEB_ID")
+unsetenv("MLM_WEB_USER_CRED")
+
 parfor idx = 1:numel(in)
     out(idx) = 2*in(idx);
 end
+disp(out);
+parpool
